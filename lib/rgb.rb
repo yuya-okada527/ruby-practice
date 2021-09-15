b3d0b71199263345a2caebf1a347deb61fb490a8
yuya-okada527 <yuya.okada527@gmail.com>
2021-09-15 22:19:07 +0900

    refactor rgb code

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,8 +1,9 @@
 def to_hex(r, g, b)
-  "##{r.to_s(16).rjust(2, "0")}#{g.to_s(16).rjust(2, "0")}#{b.to_s(16).rjust(2, "0")}"
+  [r, g, b].inject("#") do |hex, n|
+    hex += n.to_s(16).rjust(2, "0")
+  end
 end
 
 def to_ints(color_code)
-  codes = color_code.slice(1, 2), color_code.slice(3, 2), color_code.slice(5, 2)
-  codes.map { |i| i.to_i(16) }
+  color_code.scan(/\w\w/).map(&:hex)
 end
