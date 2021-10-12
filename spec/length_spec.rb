e1bf94c0ca5e90509998187898b8f6f6920670aa
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-12 23:12:43 +0900

    add length test

--- spec/length_spec.rb
+++ spec/length_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+RSpec.describe '#convert_length' do
+  [
+    {it: 'm->in', length: 1, from: :m, to: :in, expected: 39.37},
+    {it: 'in->m', length: 15, from: :in, to: :m, expected: 0.38},
+    {it: 'ft->m', length: 35000, from: :ft, to: :m, expected: 10670.73},
+  ].each do |test|
+    it test[:it] do
+      expect(convert_length(test[:length], from: test[:from], to: test[:to])).to eq test[:expected]
+    end
+  end
+end
