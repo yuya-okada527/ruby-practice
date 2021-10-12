f2250a2ab19f4f07b97f94caeff1471fc6674c45
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-12 23:04:42 +0900

    parametrized fizzbuzz tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe '#fizzbuzz' do
-  context '15の倍数の場合' do
-    it 'fizzbuzzを返す' do
-      expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  [
+    {context: '15の倍数の場合', it: 'fizzbuzzを返す', num: 45, expected: 'fizzbuzz'},
+    {context: '5の倍数の場合', it: 'buzzを返す', num: 10, expected: 'buzz'},
+    {context: '3の倍数の場合', it: 'fizzを返す', num: 6, expected: 'fizz'},
+    {context: 'その他の場合', it: 'そのまま返す', num: 2, expected: '2'},
+  ].each do |test|
+    context test[:context] do
+      it test[:it] do
+        expect(fizzbuzz(test[:num])).to eq test[:expected]
+      end
     end
   end
-  context '5の倍数の場合' do
-    it 'fizzを返す' do
-      expect(fizzbuzz(10)).to eq 'buzz'
-    end
-  end
-  context '3の倍数の場合' do
-    it 'buzzを返す' do
-      expect(fizzbuzz(6)).to eq 'fizz'
-    end
-  end
-  context 'それ以外' do
-    it 'そのまま返す' do
-      expect(fizzbuzz(2)).to eq '2'
-    end
-  end
-
 end
