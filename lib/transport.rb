7eab322063ad6791cc3ad1ef09f01174c37541af
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-13 00:14:47 +0900

    refactor transport

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,27 +1,26 @@
-STATIONS = {
-  umeda: 1,
-  juso: 2,
-  mikuni: 3
-}
-
 class Gate
+  STATIONS = {
+    umeda: 1,
+    juso: 2,
+    mikuni: 3
+  }
   attr_accessor :station
   def initialize(station)
-    self.station = station
+    @station = station
   end
 
   def enter(ticket)
-    ticket.enter = self.station
+    ticket.stamp(@station)
   end
 
   def exit(ticket)
-    ticket.fee >= required_fee(ticket)
+    ticket.fare >= required_fee(ticket)
   end
 
   private
 
   def required_fee(ticket)
-    section = STATIONS[self.station] - STATIONS[ticket.enter]
+    section = STATIONS[@station] - STATIONS[ticket.stamped_at]
     if section == 1
       150
     else
@@ -31,8 +30,16 @@ class Gate
 end
 
 class Ticket
-  attr_accessor :fee, :enter
-  def initialize(fee)
-    self.fee = fee
+  attr_accessor :fare
+  def initialize(fare)
+    @fare = fare
+  end
+
+  def stamp(station)
+    @enter = station
+  end
+
+  def stamped_at
+    @enter
   end
 end
